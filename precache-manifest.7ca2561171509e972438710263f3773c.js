b647066084ed25a094156e0a25181e178edbe568
Rahul Khatri <forever_rahul16@hotmail.com>
2020-11-06 15:39:39 +1300

    Updates

---
+++ precache-manifest.7ca2561171509e972438710263f3773c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5c714ccc5069253ad781ed51503d315",
+    "url": "/React-App-MyToDoList/index.html"
+  },
+  {
+    "revision": "1ee07fc77dcecbaf07e3",
+    "url": "/React-App-MyToDoList/static/css/main.d3e23964.chunk.css"
+  },
+  {
+    "revision": "6e7d4dfb6e88e465b1f2",
+    "url": "/React-App-MyToDoList/static/js/2.dd3b4fa4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-App-MyToDoList/static/js/2.dd3b4fa4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ee07fc77dcecbaf07e3",
+    "url": "/React-App-MyToDoList/static/js/main.0ab7ea81.chunk.js"
+  },
+  {
+    "revision": "bf8ce352f3ec4a309357",
+    "url": "/React-App-MyToDoList/static/js/runtime-main.a42a3f50.js"
+  }
+]);
